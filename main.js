15804d63c6361d933dae21ae04fe521c513e5b2c
Callylove <106864121+Callylove@users.noreply.github.com>
2023-09-20 18:02:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const price = document.getElementById('price');
+const movieSelect = document.getElementById('movie');
+var selectedMovieDiv = document.getElementById('selected-movie');
+let ticketPrice = +movieSelect.value;
+
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  const selectedMoviePrice = localStorage.getItem('selectedMoviePrice');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+
+  if (selectedMoviePrice !== null) {
+    count.innerText = selectedSeats.length;
+    price.innerText = selectedSeats.length * +selectedMoviePrice;
+  }
+};
+
+populateUI();
+
+selectedMovie = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
+const updateSelectedSeatsCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  price.innerText = selectedSeatsCount * ticketPrice;
+};
+
+// Seat select event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedSeatsCount();
+  }
+});
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+ 
+
+  ticketPrice = +e.target.value;
+  selectedMovie(e.target.selectedIndex, e.target.value);
+  var selectedOption = movieSelect.options[movieSelect.selectedIndex];
+  var radioButton = document.createElement('input');
+  radioButton.type = 'hidden';
+  radioButton.name = 'selectedMovie';
+  radioButton.value = selectedOption.value;
+  var label = document.createElement('label');
+  label.textContent = selectedOption.text;
+  selectedMovieDiv.innerHTML = '';
+  selectedMovieDiv.appendChild(radioButton);
+  selectedMovieDiv.appendChild(label);
+ 
+  updateSelectedSeatsCount();
+});
+movieSelect.dispatchEvent(new Event('change'));
